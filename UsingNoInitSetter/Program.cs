faa4b42a09c568c9fe233c3c0119cac13164a97c
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-13 10:48:41 +0000

    Add project files.

---
+++ UsingNoInitSetter/Program.cs
@@ -0,0 +1,23 @@
+﻿using UsingNoInitSetter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Using Non-Init-Setter - Regular way!");
+
+        Member memberObj = new Member();
+        memberObj.Id = 1;
+        memberObj.Name = "Abdul Salam";
+        memberObj.Address = "Kerala, India";
+
+        Console.WriteLine("****************START - Member Details***********");
+
+        Console.WriteLine($"ID : -  { memberObj.Id}");
+        Console.WriteLine($"Name :-  {memberObj.Name}");
+        Console.WriteLine($"Address:-   {memberObj.Address}");
+        Console.ReadLine();
+
+        Console.WriteLine("****************END - Member Details***********");
+    }
+}
